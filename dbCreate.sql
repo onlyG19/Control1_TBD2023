aadd2d76df8192893ab0e31a792e08af4b6e96ba
Byron Gracia <byron.gracia@usach.cl>
2023-09-20 14:18:09 -0300

    [Byron Gracia] Se agregan nuevos Insert para contemplar anios distintos a 2023, refactor al archivos del proyecto

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,5 +1,4 @@
--- Debe crearse una base de datos, por ejemplo con el nombre 'control1'
-
+--Debe crearse una base de datos, por ejemplo con el nombre "control1"
 
 CREATE TABLE IF NOT EXISTS Rol(
     id_rol serial PRIMARY KEY,
@@ -12,18 +11,16 @@ CREATE TABLE IF NOT EXISTS Curso(
     letra char(1)
 );
 
-
-CREATE TABLE IF NOT EXISTS Comuna (
+CREATE TABLE IF NOT EXISTS Comuna(
     id_comuna serial PRIMARY KEY,
     nombre varchar(30)
 );
 
-
-CREATE TABLE IF NOT EXISTS Colegio (
+CREATE TABLE IF NOT EXISTS Colegio(
     id_colegio serial PRIMARY KEY,
+    id_comuna serial,
     nombre varchar(30),
-    id_comuna serial REFERENCES Comuna(id_comuna)
-  
+    FOREIGN KEY (id_comuna) REFERENCES Comuna(id_comuna)
 );
 
 CREATE TABLE IF NOT EXISTS Franja_Horaria(
@@ -33,28 +30,30 @@ CREATE TABLE IF NOT EXISTS Franja_Horaria(
     dia date
 );
 
-CREATE TABLE IF NOT EXISTS Empleado (
+CREATE TABLE IF NOT EXISTS Empleado(
     id_empleado serial PRIMARY KEY,
+    id_comuna serial,
+    id_colegio serial,
     sueldo money,
-    id_comuna serial REFERENCES Comuna(id_comuna),
-    id_colegio serial REFERENCES Colegio(id_colegio)
-
+    FOREIGN KEY (id_comuna) REFERENCES Comuna(id_comuna),
+    FOREIGN KEY (id_colegio) REFERENCES Colegio(id_colegio)
 );
 
--- Crear la tabla 'Profesor'
-CREATE TABLE IF NOT EXISTS Profesor (
+CREATE TABLE IF NOT EXISTS Profesor(
     id_profesor serial PRIMARY KEY,
+    id_empleado serial,
     nombre varchar(30),
     apellido varchar(30),
-    id_empleado serial REFERENCES Empleado(id_empleado)
+    FOREIGN KEY (id_empleado) REFERENCES Empleado(id_empleado)
 );
 
 CREATE TABLE IF NOT EXISTS Profesor_Curso(
     id_profesor_curso serial PRIMARY KEY,
+    id_profesor serial,
+    id_curso serial,
     profesor_jefe boolean,
-    id_profesor serial REFERENCES Profesor(id_profesor),
-    id_curso serial REFERENCES Curso(id_curso)
-    
+    FOREIGN KEY (id_profesor) REFERENCES Profesor(id_profesor),
+    FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
 );
 
 CREATE TABLE IF NOT EXISTS Profesor_Curso_Franja(
@@ -62,44 +61,44 @@ CREATE TABLE IF NOT EXISTS Profesor_Curso_Franja(
     id_franja serial,
     PRIMARY KEY (id_profesor_curso, id_franja),
     FOREIGN KEY (id_profesor_curso) REFERENCES Profesor_Curso(id_profesor_curso),
-    FOREIGN KEY (id_franja) REFERENCES Franja_Horaria(id_franja)
+    FOREIGN KEY (id_franja) REFERENCES franja_horaria(id_franja)
 );
 
 CREATE TABLE IF NOT EXISTS Alumno(
     id_alumno serial PRIMARY KEY,
+    id_colegio serial,
+    id_comuna serial,
     nombre varchar(30),
     apellido varchar(30),
-    id_colegio serial REFERENCES Colegio(id_colegio),
-    id_comuna serial REFERENCES Comuna(id_comuna)
-    
+    FOREIGN KEY (id_colegio) REFERENCES Colegio(id_colegio),
+    FOREIGN KEY (id_comuna) REFERENCES Comuna(id_comuna)
 );
-
 
 CREATE TABLE IF NOT EXISTS Franja_Alumno(
-    id_franja serial REFERENCES Franja_Horaria(id_franja),
-    id_alumno serial REFERENCES Alumno(id_alumno),
+    id_franja serial,
+    id_alumno serial,
     asistencia boolean,
-    PRIMARY KEY (id_franja, id_alumno)
+    PRIMARY KEY (id_franja, id_alumno),
+    FOREIGN KEY (id_franja) REFERENCES Franja_Horaria(id_franja),
+    FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
 );
-
-
 
 CREATE TABLE IF NOT EXISTS Curso_Alumno(
-    id_alumno serial REFERENCES Alumno(id_alumno),
-    id_curso serial REFERENCES Curso(id_curso),
-    anio int
+    id_alumno serial,
+    id_curso serial,
+    anio int,
+    FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno),
+    FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
 );
-
-
 
 CREATE TABLE IF NOT EXISTS Apoderado(
     id_apoderado serial PRIMARY KEY,
+    id_comuna serial,
     nombre varchar(30),
     apellido varchar(30),
-    sexo varchar(1),
-    id_comuna serial REFERENCES Comuna(id_comuna)
+    sexo char(1),
+    FOREIGN KEY (id_comuna) REFERENCES Comuna(id_comuna)
 );
-
 
 CREATE TABLE IF NOT EXISTS Alumno_Apoderado(
     id_alumno serial,
@@ -110,7 +109,6 @@ CREATE TABLE IF NOT EXISTS Alumno_Apoderado(
     FOREIGN KEY (id_apoderado) REFERENCES Apoderado(id_apoderado)
 );
 
-
 CREATE TABLE IF NOT EXISTS Empleado_Rol(
     id_empleado serial,
     id_rol serial,
@@ -119,9 +117,10 @@ CREATE TABLE IF NOT EXISTS Empleado_Rol(
     FOREIGN KEY (id_rol) REFERENCES Rol(id_rol)
 );
 
-CREATE TABLE IF NOT EXISTS Empleado_Administrativo (
+CREATE TABLE IF NOT EXISTS Empleado_Administrativo(
     id_administrativo serial PRIMARY KEY,
+    id_empleado serial,
     nombre varchar(30),
     apellido varchar(30),
-    id_empleado serial REFERENCES Empleado(id_empleado)
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
